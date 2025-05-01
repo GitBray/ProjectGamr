5eef310f1d6e739bd4b6352842833f54c952f3a2
NathanVanhoof <101292878+ShadowWolf2403@users.noreply.github.com>
2025-05-01 13:28:11 -0500

    added the ability to get a profile pic from gallery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,10 @@ dependencies {
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.google.android.gms:play-services-location:21.1.0")
     implementation("com.google.android.gms:play-services-maps:17.0.1") // Google Maps
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
 
 }
